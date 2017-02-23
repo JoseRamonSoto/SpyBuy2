b0ce8873763e1d7924f8d676c2d733938429aa6d
Jose Ramon Soto <student@joses-macbook.local>
2017-02-23 09:22:21 -0600

    Initial Commit

---
+++ SpyBuy/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  SpyBuy
+//
+//  Created by ryala on 2/22/17.
+//  Copyright © 2017 Barrington Highschool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
+{
+    @IBOutlet weak var myWishlist: UITableView!
+    
+    var wishes = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        myWishlist.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return wishes.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        var cell:UITableViewCell = self.tableView.de
+        
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        <#code#>
+    }
+
+
+}
+
