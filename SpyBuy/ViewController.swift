ac3530dee2181f86f675a1c8c4915a95dc5af6c6
Jose Ramon Soto <student@joses-macbook.local>
2017-02-24 09:50:10 -0600

    Commit

--- SpyBuy/ViewController.swift
+++ SpyBuy/ViewController.swift
@@ -12,32 +12,74 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 {
     @IBOutlet weak var myWishlist: UITableView!
     
+    
+    
+
+    
     var wishes = [String]()
     
-    var testWishes = ["puppy", "PS4", "Laptop"]
+    var testWishes = [" puppy", "PS4", "Laptop"]
 
-    override func viewDidLoad() {
+    override func viewDidLoad () {
         super.viewDidLoad()
         
         myWishlist.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        if wishes.count == 0{
+            addGift()
+        }
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return testWishes.count
+        return wishes.count
     }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
-    {
-        var cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell")! as UITableViewCell
-        cell.textLabel?.text = self.testWishes[indexPath.row]
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell")! as UITableViewCell
+        cell.textLabel?.text = self.wishes[indexPath.row]
         
         return cell
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
-    {
-        print("fuck you")
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        print("yo")
     }
+    
+    func addGift(){
+        let alertController = UIAlertController(title: "gift", message: "What gift do you want?", preferredStyle: .alert)
+        
+        let confirmAction = UIAlertAction(title: "Confirm", style: .default) { (_) in
+            if let field = alertController.textFields?[0] {
+                // store your data
+                self.wishes.append(field.text!)
+                self.myWishlist.reloadData()
+            } else {
+                // user did not fill field
+                print("User did not fill")
+            }
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (_) in }
+        
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Enter what gift you'd like!"
+        }
+        
+        alertController.addAction(confirmAction)
+        alertController.addAction(cancelAction)
+        
+        self.present(alertController, animated: true, completion: nil)
 
+
+    }
+    
+
+    @IBAction func addButtonTapped(_ sender: Any) {
+        addGift()
+    }
+    
+    
 
 }
 
